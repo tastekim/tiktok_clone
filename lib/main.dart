9726270e99d879ecfbcd5bab603acb3e98fa4be7
tastekim <tastekim@kakao.com>
2023-05-03 02:41:43 +0900

    :art: Gaps, Sizes class 추가

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+import 'constants/gaps.dart';
+import 'constants/sizes.dart';
+
 void main() {
   runApp(const TikTokApp());
 }
@@ -15,7 +18,18 @@ class TikTokApp extends StatelessWidget {
       theme: ThemeData(
         primaryColor: const Color(0xFFE9435A),
       ),
-      home: Container(),
+      home: Padding(
+        padding: const EdgeInsets.all(Sizes.size14),
+        child: Container(
+          child: Row(
+            children: const [
+              Text('hello'),
+              Gaps.h20,
+              Text('hello'),
+            ],
+          ),
+        ),
+      )
     );
   }
 }
