271bba6a48d32732dddc837d954b114b9aae7cc5
tastekim <tastekim@kakao.com>
2023-05-06 02:06:31 +0900

    :sparkles: Complete login_form_screen.dart

--- lib/features/authentication/login_form_screen.dart
+++ lib/features/authentication/login_form_screen.dart
@@ -1,11 +1,103 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:tiktok_clone/constants/gaps.dart';
+import 'package:tiktok_clone/features/authentication/widgets/form_button.dart';
 
-class LoginFormScreen extends StatelessWidget {
+import '../../constants/sizes.dart';
+
+class LoginFormScreen extends StatefulWidget {
   const LoginFormScreen({Key? key}) : super(key: key);
 
   @override
+  State<LoginFormScreen> createState() => _LoginFormScreenState();
+}
+
+class _LoginFormScreenState extends State<LoginFormScreen> {
+  final GlobalKey<FormState> _formkey = GlobalKey<FormState>();
+  Map<String, String> formData = {};
+
+  void _onSubmitTap() {
+    if (_formkey.currentState != null) {
+      if (_formkey.currentState!.validate()) {
+        _formkey.currentState!.save();
+        print(formData);
+      }
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Log in'),
+      ),
+      body: Padding(
+        padding: EdgeInsets.symmetric(
+          horizontal: Sizes.size36,
+        ),
+        child: Form(
+          key: _formkey,
+          child: Column(
+            children: [
+              Gaps.v28,
+              TextFormField(
+                decoration: InputDecoration(
+                  hintText: 'Email',
+                  enabledBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.grey.shade400,
+                    ),
+                  ),
+                  focusedBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.grey.shade400,
+                    ),
+                  ),
+                ),
+                cursorColor: Theme.of(context).primaryColor,
+                validator: (value) {
+                  return null;
+                },
+                onSaved: (newValue) {
+                  if (newValue != null) {
+                    formData['email'] = newValue;
+                  }
+                },
+              ),
+              Gaps.v16,
+              TextFormField(
+                decoration: InputDecoration(
+                  hintText: 'Password',
+                  enabledBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.grey.shade400,
+                    ),
+                  ),
+                  focusedBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.grey.shade400,
+                    ),
+                  ),
+                ),
+                cursorColor: Theme.of(context).primaryColor,
+                validator: (value) {
+                  return null;
+                },
+                onSaved: (newValue) {
+                  if (newValue != null) {
+                    formData['password'] = newValue;
+                  }
+                },
+              ),
+              Gaps.v28,
+              GestureDetector(
+                onTap: _onSubmitTap,
+                child: FormButton(disabled: false),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
--- lib/features/authentication/login_screen.dart
+++ lib/features/authentication/login_screen.dart
@@ -3,13 +3,22 @@ import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:tiktok_clone/constants/gaps.dart';
 import 'package:tiktok_clone/constants/sizes.dart';
+import 'package:tiktok_clone/features/authentication/login_form_screen.dart';
 import 'package:tiktok_clone/features/authentication/widgets/auth_button.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({Key? key}) : super(key: key);
 
-  void _onSignupTap (BuildContext context) {
+  void _onSignupTap(BuildContext context) {
     Navigator.of(context).pop();
+  }
+
+  void _onEmailLoginTap(BuildContext context) {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (context) => const LoginFormScreen(),
+      ),
+    );
   }
 
   @override
@@ -21,7 +30,7 @@ class LoginScreen extends StatelessWidget {
             horizontal: Sizes.size40,
           ),
           child: Column(
-            children: const [
+            children: [
               Gaps.v80,
               Text(
                 'Log in to TikTok',
@@ -40,9 +49,12 @@ class LoginScreen extends StatelessWidget {
                 textAlign: TextAlign.center,
               ),
               Gaps.v40,
-              AuthButton(
-                icon: FaIcon(FontAwesomeIcons.user),
-                text: 'Use email & password',
+              GestureDetector(
+                onTap: () => _onEmailLoginTap(context),
+                child: AuthButton(
+                  icon: FaIcon(FontAwesomeIcons.user),
+                  text: 'Use email & password',
+                ),
               ),
               Gaps.v16,
               AuthButton(
