973cf3cad701b97c3f1b9a49a3faaaa6cf3e140c
tastekim <tastekim@kakao.com>
2023-07-11 22:58:10 +0900

    :bulb: study Animation Controller

--- lib/features/videos/widgets/video_post.dart
+++ lib/features/videos/widgets/video_post.dart
@@ -5,7 +5,7 @@ import 'package:tiktok_clone/constants/sizes.dart';
 import 'package:video_player/video_player.dart';
 import 'package:visibility_detector/visibility_detector.dart';
 
-class VideoPost extends StatefulWidget {
+class VideoPost extends StatefulWidget{
   final Function onVideoFinished;
   final int index;
 
@@ -19,9 +19,14 @@ class VideoPost extends StatefulWidget {
   State<VideoPost> createState() => _VideoPostState();
 }
 
-class _VideoPostState extends State<VideoPost> {
+class _VideoPostState extends State<VideoPost> with SingleTickerProviderStateMixin{
   final VideoPlayerController _videoPlayerController =
       VideoPlayerController.asset('assets/videos/video.MOV');
+
+  late final AnimationController _animationController;
+
+  bool _isPaused = false;
+  final Duration _animationDuration = Duration(milliseconds: 200);
 
   void _onVideoChange() {
     if (_videoPlayerController.value.isInitialized) {
@@ -42,6 +47,17 @@ class _VideoPostState extends State<VideoPost> {
   void initState() {
     super.initState();
     _initVideoPlayer();
+
+    _animationController = AnimationController(
+      vsync: this,
+      lowerBound: 1.0,
+      upperBound: 1.5,
+      value: 1.5,
+      duration: _animationDuration,
+    );
+    _animationController.addListener(() {
+      setState(() {});
+    });
   }
 
   @override
@@ -59,9 +75,14 @@ class _VideoPostState extends State<VideoPost> {
   void _onTogglePause() {
     if (_videoPlayerController.value.isPlaying) {
       _videoPlayerController.pause();
+      _animationController.reverse();
     } else {
       _videoPlayerController.play();
+      _animationController.forward();
     }
+    setState(() {
+      _isPaused = !_isPaused;
+    });
   }
 
   @override
@@ -84,12 +105,19 @@ class _VideoPostState extends State<VideoPost> {
             ),
           ),
           Positioned.fill(
-            child: IgnorePointer(
-              child: Center(
-                child: FaIcon(
-                  FontAwesomeIcons.play,
-                  color: Colors.white,
-                  size: Sizes.size52,
+            child: Transform.scale(
+              scale: _animationController.value,
+              child: AnimatedOpacity(
+                opacity: _isPaused ? 1 : 0,
+                duration: _animationDuration,
+                child: IgnorePointer(
+                  child: Center(
+                    child: FaIcon(
+                      FontAwesomeIcons.play,
+                      color: Colors.white,
+                      size: Sizes.size52,
+                    ),
+                  ),
                 ),
               ),
             ),
